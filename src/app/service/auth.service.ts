5c04b025d8db55e329621fa4adb77223cd1ef2f1
Kevelly <kevelly133@gmail.com>
2021-06-01 01:21:12 -0300

    Complementando os componentes de logar e cadastrar

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  entrar (userLogin: UserLogin): Observable<UserLogin>{
+return this.http.post<UserLogin>('https://blogkev.herokuapp.com/usuarios/logar', userLogin)
+  }
+  cadastrar (user:User): Observable<User> {
+return this.http.post<User>('https://blogkev.herokuapp.com/usuarios/cadastrar', user)
+  }
 }
