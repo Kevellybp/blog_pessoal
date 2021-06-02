a164fcab3aa609e6b9b5e53a49b42250980afcbb
Kevelly <kevelly133@gmail.com>
2021-06-01 22:51:52 -0300

    Adicionando componentes Inicio e Tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -3,11 +3,11 @@ import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  selector: 'app-tema',
+  templateUrl: './tema.component.html',
+  styleUrls: ['./tema.component.css']
 })
-export class InicioComponent implements OnInit {
+export class TemaComponent implements OnInit {
 
   constructor(
     private router: Router
@@ -15,10 +15,9 @@ export class InicioComponent implements OnInit {
 
   ngOnInit() {
     if (environment.token == ''){
-     // alert ('Sua sessão expirou, faça o login novamente!')
-      this.router.navigate(['/entrar'])
-    }
+    
+       this.router.navigate(['/entrar'])
+     }
   }
 
 }
- 
