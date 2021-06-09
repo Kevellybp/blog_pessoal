29bc844d6d9c738fcd92622542800b20ddf72032
Kevelly <kevelly133@gmail.com>
2021-06-09 01:12:45 -0300

    Implementando delete de postagem

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -20,9 +20,20 @@ getAllPostagens(): Observable<Postagem[]>{
   return this.http.get<Postagem[]>('https://blogkev.herokuapp.com/postagens', this.token)
 }
 
+getByIdPostagem(id: number): Observable<Postagem>{
+  return this.http.get<Postagem>(`https://blogkev.herokuapp.com/postagens/${id}`, this.token)
+}
+
 postPostagem(postagem: Postagem): Observable<Postagem>{
   return this.http.post<Postagem>('https://blogkev.herokuapp.com/postagens', postagem, this.token)
 }
 
+putPostagem(postagem: Postagem): Observable<Postagem>{
+  return this.http.put<Postagem>('https://blogkev.herokuapp.com/postagens', postagem, this.token)
+
+}
+deletePostagem(id: number){
+  return this.http.delete(`https://blogkev.herokuapp.com/postagens/${id}`, this.token)
+}
 
 }
